5655437299ff5a858dcf273590865c6771f3c1d2
dawnswwwww <wxn_em@163.com>
2022-04-13 00:52:52 +0800

    add init

---
+++ server/index.mjs
@@ -0,0 +1,29 @@
+import { Logger } from '@hocuspocus/extension-logger';
+import { Server } from '@hocuspocus/server';
+import { slateNodesToInsertDelta } from '@slate-yjs/core';
+import * as Y from 'yjs';
+
+const initialValue = [{ type: 'paragraph', children: [{ text: '' }] }];
+
+// Setup the server
+const server = Server.configure({
+  port: 1234,
+
+  // Add logging
+  extensions: [new Logger()],
+
+  async onLoadDocument(data) {
+    // Load the initial value in case the document is empty
+    if (data.document.isEmpty('content')) {
+      const insertDelta = slateNodesToInsertDelta(initialValue);
+      const sharedRoot = data.document.get('content', Y.XmlText);
+      sharedRoot.applyDelta(insertDelta);
+    }
+
+    return data.document;
+  },
+});
+
+// Start the server
+server.enableMessageLogging();
+server.listen();
---
+++ web/App.jsx
@@ -0,0 +1,7 @@
+import Editor from './pages/Editor'
+
+const App = () => {
+  return <div><Editor /></div>
+}
+
+export default App;
